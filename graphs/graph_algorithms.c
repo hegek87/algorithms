b2175c230fc6fbb00581a93e1e54cb5b1a843951
hegek87 <hegelandka@vcu.edu>
2014-01-14 17:00:02 -0500

    Created graphs project - Going to implement Dijkstra's algorithm

---
+++ graphs/graph_algorithms.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+	printf("HI\n");
+	return 0;
+}
---
+++ graphs/graph_algorithms.h
@@ -0,0 +1,4 @@
+#ifndef GRAPH_ALGORITHMS_H
+#define GRAPH_ALGORITHMS_H 
+#include " ../../Data-Structures/double_linked_list/double_linked_list.h"
+#endif
